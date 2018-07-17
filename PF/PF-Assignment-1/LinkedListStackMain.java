2c10b903c8b582048aeec2a8fa583a5d3eb6d0d7
meta-ayushi-khandelwal <41181775+meta-ayushi-khandelwal@users.noreply.github.com>
2018-07-17 19:56:18 +0530

    Update LinkedListStackMain.java

--- PF/PF-Assignment-1/LinkedListStackMain.java
+++ PF/PF-Assignment-1/LinkedListStackMain.java
@@ -2,43 +2,40 @@ import java.util.*;
 class Node
 {
 	int data;
-	Node next;
-	
+	Node next;	
 	public Node(int data)
 	{
-		this.data=data;
+		this.data = data;
 	}
 }
 class LinkListStack
 {
-	Node top=null;
+	Node top = null;
 	public void push(int data)
 	{
-		Node newNode=new Node(data);
-		newNode.next=top;
-		top=newNode;
+		Node newNode = new Node(data);
+		newNode.next = top;
+		top = newNode;
 	}
 	public void pop()
 	{
 		if(isEmpty())
 		{
-			System.out.print("Stack underflow");
-			System.out.print("\n");	
+			System.out.println("Stack underflow");
 		}
 		else
 		{
-			Node deletedNode=top;
+			Node deletedNode = top;
 			top=top.next;
-			System.out.println("Popped item is: "+deletedNode.data);
+			System.out.println("Popped item is: " + deletedNode.data);
 		}
-		
 	}
 	public void top()
 	{
 		if(isEmpty())
 			System.out.println("List is empty!");
 		else
-			System.out.println("Value on top is: "+top.data);		
+			System.out.println("Value on top is: " + top.data);		
 	}
 	public void display()
 	{
@@ -46,49 +43,57 @@ class LinkListStack
 			System.out.println("List is empty!");
 		else
 		{
-			Node current=top;
-			while(current!=null)
+			Node current = top;
+			while(current != null)
 			{
-				System.out.print(current.data+" ");;
-				current=current.next;
+				System.out.println("Values stack contains: " + current.data + " ");
+				current = current.next;
 			}
 		}
 	}
 	public boolean isEmpty()
 	{
-		return(top==null);
+		return(top == null);
 	}
 }
-class LinkListStackMain
+public class LinkListStackMain
 {
 	public static void main(String args[])
 	{
-		LinkListStack stack=new LinkListStack();
-		Scanner sc=new Scanner(System.in);
+		LinkListStack stack = new LinkListStack();
+		Scanner sc = new Scanner(System.in);
 		char ch;
 		do
 		{
-			System.out.println("Enter your choice:\n 1.Push\n 2.Pop\n 3.Display the top\n 4.Display the stack\n 5.Check stack is empty or not?\n");
-			int choice=sc.nextInt();
+			System.out.println("--STACK OPERATIONS--");
+			System.out.println("1.Push");
+			System.out.println("2.Pop");
+			System.out.println("3.Display the top");
+			System.out.println("4.Display the stack");
+			System.out.println("5.Check stack is empty or not?");
+			System.out.println("6.Exit");			
+			System.out.print("Enter your choice: ");
+			int choice = sc.nextInt();
 			switch(choice)
 			{
-				case 1: 
-					System.out.println("Enter what value to be pushed");
-					stack.push(sc.nextInt()); break;
-				case 2: 
-					stack.pop(); break;
-				case 3: 
-					stack.top(); break;
-				case 4:
-					stack.display(); break;	
-				case 5: 
-					System.out.println("List is empty: "+stack.isEmpty()); break;
+				case 1: System.out.print("Enter what value to be pushed: ");
+						stack.push(sc.nextInt()); break;
+				case 2: stack.pop(); break;
+				case 3: stack.top(); break;
+				case 4: stack.display(); break;	
+				case 5: System.out.println("List is empty: " + stack.isEmpty()); break;
+				case 6: return;
 				default: System.out.println("Invalid input");			
 			}
-			System.out.println("\n\nDo you want to continue (Y/N): ");
-			ch=sc.next().charAt(0);	
+			System.out.print("Press 'Y' for continue and 'N' for exit: ");
+			ch=sc.next().charAt(0);
+			while(!(ch=='Y' || ch=='y' || ch=='N' || ch=='n'))
+			{
+				System.out.println("Invalid input");
+				System.out.print("Press 'Y' for continue and 'N' for exit: ");
+				ch=sc.next().charAt(0);
+			}	
+			System.out.print("\n");
 		}while(ch=='Y' || ch=='y');
-		
 	}
-
 }
