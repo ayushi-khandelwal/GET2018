e107f7da215267c0def85fa03b85399b18e9fa2a
meta-ayushi-khandelwal <41181775+meta-ayushi-khandelwal@users.noreply.github.com>
2018-07-17 20:05:46 +0530

    Update PalindromeUsingStack.java

--- PF/PF-Assignment-1/PalindromeUsingStack.java
+++ PF/PF-Assignment-1/PalindromeUsingStack.java
@@ -1,30 +1,45 @@
 import java.util.Scanner;
 class Stack
 {
-	int top=-1;
+	int top = -1;
 	char arr[];
-	Stack()
+	Stack()				//default constructor
 	{
-		arr=new char[20];
+		arr = new char[20];
 	}
 	
-	void push(char val)
+	public void push(char val)
 	{
-		arr[++top]=val;
+		arr[++top] = val;
 	}
-	void pop(char val)
+	public void pop(char val)
 	{
-		if(arr[top]==val && top!=-1)
+		if(arr[top] == val && top != -1)
 		{
 			top--;
 		}	
 	}
-	void check()
+	public void check()	//check all the strings except the single character string
 	{
-		if(top==-1)
+		if(top == -1)
 		System.out.println("Palindrome!");
 		else
 		System.out.println("Not palindrome!");
+ 	}
+ 	public boolean checkSingleString()	//check a single character only
+	{
+		if(top == -1)
+		{
+			System.out.println("Palindrome!"); return true;
+		}
+		else return false;
+ 	}
+	public boolean isEmpty()
+	{
+		if(top == -1)
+		return true;
+		else
+		return false;
 	}
 }
 class PalindromeUsingStack
@@ -32,16 +47,30 @@ class PalindromeUsingStack
 	public static void main(String args[])
 	{
 		Stack stack=new Stack();
-		System.out.println("Enter the string to check it a palindrome or not!");
-		Scanner sc=new Scanner(System.in); 
-		String str= sc.next();
-		
-		for(int i=0;i<(str.length())/2;i++)
-			stack.push(str.charAt(i));	
-		for(int i=((str.length())/2); i<str.length();i++)
+		char ch='a';
+		do
 		{
-			stack.pop(str.charAt(i));
-		}	
-		stack.check();
+			System.out.println("Enter the string to check it a palindrome or not!");
+			Scanner sc=new Scanner(System.in); 
+			String str= sc.next();
+			
+			for(int i=0 ; i<(str.length())/2 ; i++)		//dividing the string in halves to push the first half
+				stack.push(str.charAt(i));
+			if(stack.checkSingleString() == true) continue; 	//	
+			for(int i=(str.length()/2) ; i<str.length() ; i++)	//and pop the second half of the string
+			{
+				stack.pop(str.charAt(i));
+			}	
+			stack.check();
+			System.out.print("Press 'Y' for continue and 'N' for exit: ");
+			ch=sc.next().charAt(0);
+			while(!(ch=='Y' || ch=='y' || ch=='N' || ch=='n'))
+			{
+				System.out.println("Invalid input");
+				System.out.print("Press 'Y' for continue and 'N' for exit: ");
+				ch=sc.next().charAt(0);
+			}	
+			System.out.print("\n");
+		}while(ch=='Y' || ch=='y');
 	}	
 }
