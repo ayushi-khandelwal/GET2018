706ff304005190d7e3c47ef2664b5427e44c5df2
meta-ayushi-khandelwal <41181775+meta-ayushi-khandelwal@users.noreply.github.com>
2018-07-19 04:53:11 -0700

    Update ShoppingCart.java

--- PF/PF-Assignment-2/ShoppingCart.java
+++ PF/PF-Assignment-2/ShoppingCart.java
@@ -1,6 +1,6 @@
 import java.util.*;
 import java.util.Calendar;
-class Item
+class Item			// Item class to get details of an item
 {
 	String name;
 	int quantity;
@@ -12,22 +12,27 @@ class Item
 		this.quantity = quantity;
 		this.price = price;
 	}
+	
 	public String getName()
 	{
 		return name;
 	}
+	
 	public int getQuantity()
 	{
 		return quantity;
 	}
+	
 	public double getPrice()
 	{
 		return price;
 	}
 }
-class CartMethods
+
+class CartMethods			// cartMethods class to define all the methods required for cart
 {
 	Map<String,Item> cart = new HashMap<String,Item>();
+	
 	public void addItem()
 	{
 		Scanner scan = new Scanner(System.in);
@@ -40,12 +45,9 @@ class CartMethods
 		Item newItem = new Item(name, quantity, price);
 		cart.put(name,newItem);
 	}
-	public void removeItem()
+	
+	public void removeItem(String name)
 	{
-		
-		System.out.print("\nEnter the item name to delete : ");
-		Scanner scan = new Scanner(System.in);
-		String name = scan.next();
 		Item value = Item.class.cast(cart.get(name));
 		if(value!=null)
 		{
@@ -55,12 +57,13 @@ class CartMethods
 		else
 			System.out.println("Item not found!");
 	}
+	
 	public void updateItem()
 	{
 		System.out.print("\nEnter the item name to update : ");
 		Scanner scan = new Scanner(System.in);
 		String name = scan.next();
-		Item value = Item.class.cast(cart.get(name));
+		Item value = Item.class.cast(cart.get(name));			// casting object type output of HashMap get function to Item class
 		if(value!=null)
 		{
 			cart.remove(name);
@@ -69,6 +72,7 @@ class CartMethods
 		else
 			System.out.println("Item not found");
 	}
+	
 	public void showItems()
 	{
 		System.out.println("\nMy cart");
@@ -80,6 +84,7 @@ class CartMethods
 			System.out.println("Price : " + item.getPrice());
 		}
 	}
+	
 	public void billing(FixedProductPromotion fixedProductPromotion, FixedOrderPromotion fixedOrderPromotion)
 	{
 		int totalQuantity = 0;
@@ -87,36 +92,46 @@ class CartMethods
 		String promotionCode = "";
 		double totalDiscount = 0.0;
 		System.out.println("--PAYMENT--");
-		for(Object obj : cart.values())
+		
+		for(Object obj : cart.values())			// traversing the hashmap
 		{
 			Item item = Item.class.cast(obj);
 			totalQuantity += item.getQuantity();
 			totalPrice += item.getPrice() * item.getQuantity();
 		}
+		
 		System.out.println("Total quantity is " + totalQuantity);
 		System.out.println("Total price is " + totalPrice);
 		System.out.println("Amount payable now is " + totalPrice);
-		System.out.print("Enter promocode(if you have any) : ");
+		System.out.print("Enter promocode(if you have then enter and if you don't have then enter no)  : ");
 		Scanner scan = new Scanner(System.in);
 		promotionCode = scan.next();
+		
+		if (promotionCode.equals("no") || promotionCode.equals("NO"))
+			return;
+		
 		if(fixedOrderPromotion.isPromotionApplicable(totalPrice, promotionCode))
 		{
 			totalDiscount = totalPrice * fixedOrderPromotion.getFixedDiscount();
 		}
+		
 		else if(fixedProductPromotion.isPromotionApplicable(totalQuantity, totalPrice, promotionCode))
 		{
 			totalDiscount = totalPrice * fixedProductPromotion.getFixedDiscount();
 		}
+		
 		System.out.println("Discount applicable using promocode is " + totalDiscount);
 		System.out.println("Payable amount is " + (totalPrice-totalDiscount));
+		cart =new HashMap<String,Item>();
 	}
+		
 }
 
 interface Promotion
 {
 	enum PromotionEnum
 	{
-		get10("GET10",1,5),avail10("AVAIL10", 15,20),free10("FREE10", 25,30);
+		get10("GET10",1,5),avail10("AVAIL10", 15,20),free10("FREE10", 25,30); //set promotion code
 		String code;
 		int startDate;
 		int endDate;
@@ -127,14 +142,17 @@ interface Promotion
 			this.startDate = startDate;
 			this.endDate = endDate;
 		}
+		
 		public String getCode()
 		{
 			return code;
 		}
+		
 		public int getStartDate()
 		{
 			return startDate;
 		}
+		
 		public int getEndDate()
 		{
 			return endDate;
@@ -146,31 +164,38 @@ class FixedProductPromotion implements Promotion
 	int minQuantity; 
 	double minPrice;
 	double fixedDiscount;
+	
 	public FixedProductPromotion(int minProducts, double minPayment)
 	{
 		setMinimumPrice(minProducts, minPayment);
 		setFixedDiscount();
 	}
+	
 	public double getMinimumPrice()
 	{
 		return minPrice;
 	}
+	
 	public int getMinimumQuantity()
 	{
 		return minQuantity;
 	}
+	
 	public void setMinimumPrice(int quantity, double price)
 	{
 		minQuantity = quantity;
 		minPrice = price;
 	}
+	
 	public double getFixedDiscount()
 	{
 		return fixedDiscount;
 	}
+	
 	public void setFixedDiscount() {
 		fixedDiscount = 0.10;	//avail 10% discount
 	}
+	
 	public boolean isPromotionApplicable(int finalProducts, double totalBill, String usercode)
 	{
 		Calendar calendar = Calendar.getInstance();
@@ -191,8 +216,8 @@ class FixedProductPromotion implements Promotion
 			}
 		}
 		else
-		System.out.println("Get more products to avail discount");
-	return false;
+			System.out.println("Get more products to avail discount");
+		return false;
 	}
 }
 class FixedOrderPromotion implements Promotion
@@ -200,31 +225,38 @@ class FixedOrderPromotion implements Promotion
 	double minPrice;
 	double fixedDiscount;
 	int minQuantity;
+	
 	public FixedOrderPromotion(double minPayment)
 	{
 		setMinimumPrice(minPayment);
 		setFixedDiscount();
 	}
+	
 	public double getMinimumPrice()
 	{
 		return minPrice;
 	}
+	
 	public int getMinimumQuantity()
 	{
 		return minQuantity;
 	}
+	
 	public void setMinimumPrice(double price)
 	{
 		minPrice = price;
 	}
+	
 	public double getFixedDiscount()
 	{
 		return fixedDiscount;
 	}
+	
 	public void setFixedDiscount()
 	{
 		fixedDiscount = 0.10;	//avail 10% discount
 	}
+	
 	public boolean isPromotionApplicable(double totalBill, String usercode)
 	{
 		Calendar calendar = Calendar.getInstance();
@@ -240,24 +272,24 @@ class FixedOrderPromotion implements Promotion
 					else
 						return false;
 				}
-				else
-				System.out.println("Promocode is not available now!");
 			}
 		}
 		else
-		System.out.println("Your amount don't reach yet to avail discount");
+			System.out.println("Your amount don't reach yet to avail discount");
 		return false;
 	}
 }
+
 public class ShoppingCart
 {
 	public static void main(String args[])
 	{
 		CartMethods cartMethods = new CartMethods();
-		FixedProductPromotion fixedProductPromotion = new FixedProductPromotion(15, 2000.0);
-		FixedOrderPromotion fixedOrderPromotion = new FixedOrderPromotion(4000.0);
+		FixedProductPromotion fixedProductPromotion = new FixedProductPromotion(15, 2000.0);	//set the discount limits on basis od number of products
+		FixedOrderPromotion fixedOrderPromotion = new FixedOrderPromotion(4000.0);	//set the discount limits on basis of price
 		Scanner sc = new Scanner(System.in);
 		char proceed = 'a';
+		
 		do
 		{
 			System.out.println("\n--SHOPPING CART MENU--");			
@@ -269,24 +301,48 @@ public class ShoppingCart
 			System.out.println("6.Exit");					
 			System.out.print("Enter your choice: ");		
 			int choice = sc.nextInt();
+			
 			switch(choice)
 			{		
-				case 1: cartMethods.addItem(); break;
-				case 2: cartMethods.removeItem(); break;
-				case 3: cartMethods.updateItem(); break;
-				case 4: cartMethods.showItems(); break;
-				case 5: cartMethods.billing(fixedProductPromotion, fixedOrderPromotion); break;
-				case 6: return; 
-				default: System.out.println("Invalid input");			
+				case 1: 
+					cartMethods.addItem(); 
+					break;
+					
+				case 2: 
+					System.out.print("\nEnter the item name to delete : ");
+					String name = sc.next();
+					cartMethods.removeItem(name); 
+					break;
+					
+				case 3: 
+					cartMethods.updateItem(); 
+					break;
+					
+				case 4: 
+					cartMethods.showItems(); 
+					break;
+					
+				case 5: 
+					cartMethods.billing(fixedProductPromotion, fixedOrderPromotion);
+					break;
+					
+				case 6: 
+					return; 
+					
+				default: 
+					System.out.println("Invalid input");			
 			}
+			
 			System.out.print("\nPress 'Y' for continue and 'N' for exit: ");
 			proceed = sc.next().charAt(0);
+			
 			while(!(proceed=='Y' || proceed=='y' || proceed=='N' || proceed=='n'))
 			{
 				System.out.println("Invalid input");
 				System.out.print("Press 'Y' for continue and 'N' for exit: ");
 				proceed=sc.next().charAt(0);
 			}	
+			
 			System.out.print("\n");
 		}while(proceed=='Y' || proceed=='y');	
 	}
