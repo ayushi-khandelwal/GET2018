379be646c1b6867da1a2b6f803bd108379e0f0e1
Ayushi Khandelwal <41181775+meta-ayushi-khandelwal@users.noreply.github.com>
2018-07-24 01:55:41 -0700

    Update SearchJUnitTest.java

--- PF/SCF-Assignment-5/SearchJUnitTest.java
+++ PF/SCF-Assignment-5/SearchJUnitTest.java
@@ -1,53 +1,48 @@
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class SearchJUnitTest {
-
 	Search search = new Search();
 	
 	@Test
-	public void LinearSearchTestFirst() {
+	public void test_LinearSearch_WouldReturnCorrectOutput() {
 		assertEquals(3, search.linearSearch(new int[] {1, 9, 8, 6, 4, 3, 2}, 6, 0));
-	}
-	
-	@Test
-	public void LinearSearchTestSecond() {
-		assertEquals(0, search.linearSearch(new int[] {1, 5, 6, 8}, 1, 0));
 	}
 
 	@Test
-	public void LinearSearchTestThird() {
-		assertEquals(1, search.linearSearch(new int[] {0, 1 , 2}, 1, 0));
+	public void test_LinearSearch_WhenRepeatedElementsArePassed_ThenReturnsFirstIndexOfElement() {
+		assertEquals(1, search.linearSearch(new int[] {0, 1 ,1, 2}, 1, 0));
 	}
 
 	@Test(expected = AssertionError.class)
-	public void LinearSearchTestFourth() {
+	public void test_LinearSearch_WhenArrayIsEmpty_ReturnsAssertionError() {
 		assertEquals(-1, search.linearSearch(new int[] {}, 6, 0));
+	}
+
+	@Test(expected = ArrayIndexOutOfBoundsException.class)
+	public void test_LinearSearch_WhenElementNotFound_ReturnsArrayIndexOutOfBoundsException() {
+		assertEquals(-1, search.linearSearch(new int[] {1,2,3}, 6, 0));
 	}
 
 
 	@Test
-	public void BinarySearchTestFirst() {
+	public void test_BinarySearch_WouldReturnCorrectOutput() {
 		assertEquals(3, search.binarySearch(new int[] {1, 2, 5, 6, 8, 9, 12}, 6, 0, 6));
 	}
 	
 	@Test
-	public void BinarySearchTestSecond() {
-		assertEquals(3, search.binarySearch(new int[] {1, 2, 6, 8}, 8, 0, 3));
+	public void test_BinarySearch_WhenElementNotFound_ReturnsArrayIndexOutOfBoundsException() {
+		assertNotEquals(3, search.binarySearch(new int[] {1, 2, 6, 8}, 3, 0, 3));
 	}
 
-  	@Test
-	
-	public void BinarySearchTestThird() {
-		assertEquals(0, search.binarySearch(new int[] {0, 1 , 2}, 0, 0, 2));
+
+	@Test
+	public void test_BinarySearch_WhenRepeatedElementsArePassed_ThenReturnsFirstIndexOfElement() {
+		assertNotEquals(3, search.binarySearch(new int[] {1, 2, 2, 6, 8}, 2, 0, 3));
 	}
 
 	@Test
-	public void BinarySearchTestFourth() {
+	public void test_BinarySearch_WhenArrayIsEmpty_ReturnsAssertionError() {
 		assertEquals(-1, search.binarySearch(new int[] {}, 6, 0, 0));
 	}
-
-	
-	
 }
