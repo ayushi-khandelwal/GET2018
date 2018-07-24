1a6e9884625e71d41933f0d03c232e60f0c3c10b
Ayushi Khandelwal <41181775+meta-ayushi-khandelwal@users.noreply.github.com>
2018-07-24 01:52:44 -0700

    Update NQueenJUnitTest.java

--- PF/SCF-Assignment-5/NQueenJUnitTest.java
+++ PF/SCF-Assignment-5/NQueenJUnitTest.java
@@ -6,18 +6,40 @@ public class NQueenJUnitTest {
 	NQueenProblem nQueenProblem = new NQueenProblem();
 	
 	@Test
-	public void testFirst() {
-		assertEquals(true, nQueenProblem.nQueen(new int [][] {
-            { 0,  0,  0,  0},
-            { 0,  0,  0,  0},
-            { 0,  0,  0,  0},
-            { 0,  0,  0,  0} }, 0, 4));
+	public void test_WhenArraySizeIs4_WouldReturnSingleSolution() {
+		int[][] board = nQueenProblem.nQueenSolution(new int[][] {{ 0,  0,  0,  0},
+									 { 0,  0,  0,  0},
+									  { 0,  0,  0,  0},
+									  { 0,  0,  0,  0} }, 0, 4);
+		assertArrayEquals(new int[][] {{ 0,  0,  1,  0},
+						{ 1,  0,  0,  0},
+					        { 0,  0,  0,  1},
+					    	{ 0,  1,  0,  0} }, board);
+	}
+
+	@Test
+	public void test_WhenArraySizeIs3_ThenArrayIsNotSameAsExpected() {
+		int[][] board = nQueenProblem.nQueenSolution(new int[][] {{ 0,  0,  0 },
+									  { 0,  0,  0 },
+									  { 0,  0,  0 } }, 0, 3);
+		assertNotSame(new int[][] {{ 0,  0,  1 },
+					   { 1,  0,  0 },
+					   { 0,  0,  0 } }, board);
 		
 	}
 
 	@Test
-	public void testSecond() {
-		assertEquals(true, nQueenProblem.nQueen(new int [][] {
+	public void test_WhenArraySizeIs8_ThenArrayIsSameAsExpected() {
+		int[][] board = nQueenProblem.nQueenSolution(new int[][] {{ 1,  0,  0,  0,  0,  0,  0,  0},
+									  { 0,  0,  0,  0,  0,  0,  1,  0},
+									  { 0,  0,  0,  0,  1,  0,  0,  0},
+									  { 0,  0,  0,  0,  0,  0,  0,  1},
+									  { 0,  1,  0,  0,  0,  0,  0,  0},
+									  { 0,  0,  0,  1,  0,  0,  0,  0},
+									  { 0,  0,  0,  0,  0,  1,  0,  0},
+									  { 0,  0,  1,  0,  0,  0,  0,  0} }, 0, 8);
+		
+		assertArrayEquals(board, nQueenProblem.nQueenSolution(new int [][] {
             { 0,  0,  0,  0, 0, 0,  0, 0},
             { 0,  0,  0,  0, 0, 0,  0, 0},
             { 0,  0,  0,  0, 0, 0,  0, 0},
