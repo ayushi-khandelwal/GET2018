8d8bfd5c158644296fd43a37993825ed4f9a01ec
Ayushi Khandelwal <41181775+meta-ayushi-khandelwal@users.noreply.github.com>
2018-07-24 04:53:42 -0700

    Update NQueenJUnitTest.java

--- PF/SCF-Assignment-5/NQueenJUnitTest.java
+++ PF/SCF-Assignment-5/NQueenJUnitTest.java
@@ -1,5 +1,4 @@
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class NQueenJUnitTest {
@@ -7,48 +6,34 @@ public class NQueenJUnitTest {
 	
 	@Test
 	public void test_WhenArraySizeIs4_WouldReturnSingleSolution() {
-		int[][] board = nQueenProblem.nQueenSolution(new int[][] {{ 0,  0,  0,  0},
-									 { 0,  0,  0,  0},
-									  { 0,  0,  0,  0},
-									  { 0,  0,  0,  0} }, 0, 4);
+		int[][] board = nQueenProblem.nQueenSolution(4);
 		assertArrayEquals(new int[][] {{ 0,  0,  1,  0},
 						{ 1,  0,  0,  0},
-					        { 0,  0,  0,  1},
-					    	{ 0,  1,  0,  0} }, board);
+					       { 0,  0,  0,  1},
+					       { 0,  1,  0,  0} }, board);
+		
 	}
 
-	@Test
+	@Test(expected = AssertionError.class)
 	public void test_WhenArraySizeIs3_ThenArrayIsNotSameAsExpected() {
-		int[][] board = nQueenProblem.nQueenSolution(new int[][] {{ 0,  0,  0 },
-									  { 0,  0,  0 },
-									  { 0,  0,  0 } }, 0, 3);
-		assertNotSame(new int[][] {{ 0,  0,  1 },
-					   { 1,  0,  0 },
-					   { 0,  0,  0 } }, board);
-		
+		int[][] board = nQueenProblem.nQueenSolution( 3);
+		assertArrayEquals(new int[][] {{0, 0, 0},
+						{0, 0, 0}, 
+						{0, 0, 0}}, board);
 	}
 
 	@Test
-	public void test_WhenArraySizeIs8_ThenArrayIsSameAsExpected() {
-		int[][] board = nQueenProblem.nQueenSolution(new int[][] {{ 1,  0,  0,  0,  0,  0,  0,  0},
-									  { 0,  0,  0,  0,  0,  0,  1,  0},
-									  { 0,  0,  0,  0,  1,  0,  0,  0},
-									  { 0,  0,  0,  0,  0,  0,  0,  1},
-									  { 0,  1,  0,  0,  0,  0,  0,  0},
-									  { 0,  0,  0,  1,  0,  0,  0,  0},
-									  { 0,  0,  0,  0,  0,  1,  0,  0},
-									  { 0,  0,  1,  0,  0,  0,  0,  0} }, 0, 8);
-		
-		assertArrayEquals(board, nQueenProblem.nQueenSolution(new int [][] {
-            { 0,  0,  0,  0, 0, 0,  0, 0},
-            { 0,  0,  0,  0, 0, 0,  0, 0},
-            { 0,  0,  0,  0, 0, 0,  0, 0},
-            { 0,  0,  0,  0, 0, 0,  0, 0},
-            { 0,  0,  0,  0, 0, 0,  0, 0},
-            { 0,  0,  0,  0, 0, 0,  0, 0},
-            { 0,  0,  0,  0, 0, 0,  0, 0},
-            { 0,  0,  0,  0, 0, 0,  0, 0} }, 0, 8));
+	public void test_WhenArraySizeIs8() {
+		int[][] board = nQueenProblem.nQueenSolution( 8);
+		assertArrayEquals(new int [][] {
+				  { 1,  0,  0,  0,  0,  0,  0,  0},
+				  { 0,  0,  0,  0,  0,  0,  1,  0},
+				  { 0,  0,  0,  0,  1,  0,  0,  0},
+				  { 0,  0,  0,  0,  0,  0,  0,  1},
+				  { 0,  1,  0,  0,  0,  0,  0,  0},
+				  { 0,  0,  0,  1,  0,  0,  0,  0},
+				  { 0,  0,  0,  0,  0,  1,  0,  0},
+				  { 0,  0,  1,  0,  0,  0,  0,  0} }, board);
 		
 	}
-
 }
