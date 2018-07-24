4ccfc9acc96d85f359b45f348c21aac763993210
Ayushi Khandelwal <41181775+meta-ayushi-khandelwal@users.noreply.github.com>
2018-07-24 04:55:51 -0700

    Update NQueenProblem.java

--- PF/SCF-Assignment-5/NQueenProblem.java
+++ PF/SCF-Assignment-5/NQueenProblem.java
@@ -1,9 +1,32 @@
+
 public class NQueenProblem 
 {
-	int[][] nQueenSolution(int board[][], int col, int size)
-	{
-		nQueen(board, col, size);
-		System.out.println("N Queen Problem for " + size + " x " + size + " :  \n");
+	int[][] nQueenSolution(int size)
+    	{
+		int[][] board = init(size);	// init() will initialize board array
+		int col = 0;
+		if (nQueen(board, col, size) == true){
+			System.out.println();
+	    	System.out.println("--------------N Queen Problem for " + size + " x " + size + " :  --------------\n");
+			printArray(board, size);	//print final board
+		}
+		else 
+			throw new AssertionError();	//if number of queens can't be placed
+		
+        return board;
+    }
+
+    private int[][] init(int size) {	//method to initialize board
+    	int[][] board = new int[size][size];
+		for(int i = 0; i < size; i++){
+			for(int j = 0; j < size; j++){
+		    	board[i][j] = 0;
+			}
+		}
+		return board;
+	}
+
+	private void printArray(int[][] board, int size) {	//this method is used to print board 
 		for (int i = 0; i < size; i++)
 		{
 		    for (int j = 0; j < size; j++)
@@ -12,29 +35,27 @@ public class NQueenProblem
 		    System.out.println();
 		}
 		System.out.print("\n\n");
-		return board;
-    	}
+	}
 
-    boolean isSafe(int board[][], int row, int col, int size)
-    {
-        int i, j;
+	boolean isSafe(int board[][], int row, int col, int size) {	//checks if place of queen is safe or not
+		int i, j;
+
+		for (i = 0; i < col; i++)	// Check this row on left side 
+		    if (board[row][i] == 1)
+			return false;
+
+		for (i=row, j=col; i>=0 && j>=0; i--, j--)	// Check upper diagonal on left side 
+		    if (board[i][j] == 1)
+			return false;
+
+		for (i=row, j=col; j>=0 && i<size; i++, j--)	// Check lower diagonal on left side 
+		    if (board[i][j] == 1)
+			return false;
+
+		return true;
+	    }
  
-        for (i = 0; i < col; i++)	// Check this row on left side 
-            if (board[row][i] == 1)
-                return false;
- 
-        for (i=row, j=col; i>=0 && j>=0; i--, j--)	// Check upper diagonal on left side 
-            if (board[i][j] == 1)
-                return false;
- 
-        for (i=row, j=col; j>=0 && i<size; i++, j--)	// Check lower diagonal on left side 
-            if (board[i][j] == 1)
-                return false;
-        
-        return true;
-    }
- 
-    boolean nQueen(int board[][], int col, int size)
+    boolean nQueen(int board[][], int col, int size)	//returns true if all queens are successfully placed
     {
         if (col >= size)	//If all queens are placed then return true
             return true;
@@ -44,7 +65,8 @@ public class NQueenProblem
             if (isSafe(board, i, col, size))	// Check if the queen can be placed on board[i][col]
             {
                 board[i][col] = 1;
-
+                System.out.println("Current status of board is : ");
+                printArray(board, size);
                 if (nQueen(board, col + 1, size) == true)
                     return true;
                 
@@ -52,7 +74,7 @@ public class NQueenProblem
                 					//then remove queen from board[i][col] 
             }
         }
- 
+	    
         /* If the queen can not be placed in any row in
            this colum col, then return false */
         return false;
