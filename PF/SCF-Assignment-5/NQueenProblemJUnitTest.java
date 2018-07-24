d0a01ddfedf0f3b2b00e2dfbeb8494d97bcbf1cb
Ayushi Khandelwal <41181775+meta-ayushi-khandelwal@users.noreply.github.com>
2018-07-24 04:44:55 -0700

    Update NQueenProblemJUnitTest.java

--- PF/SCF-Assignment-5/NQueenProblemJUnitTest.java
+++ PF/SCF-Assignment-5/NQueenProblemJUnitTest.java
@@ -2,30 +2,39 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class NQueenProblemJUnitTest {
+public class NQueenJUnitTest {
 	NQueenProblem nQueenProblem = new NQueenProblem();
 	
 	@Test
-	public void testFirst() {
-		assertEquals(true, nQueenProblem.nQueen(new int [][] {
-            { 0,  0,  0,  0},
-            { 0,  0,  0,  0},
-            { 0,  0,  0,  0},
-            { 0,  0,  0,  0} }, 0, 4));
+	public void test_WhenArraySizeIs4_WouldReturnSingleSolution() {
+		int[][] board = nQueenProblem.nQueenSolution( 4);
+		assertArrayEquals(new int[][] {{ 0,  0,  1,  0},
+						{ 1,  0,  0,  0},
+					      { 0,  0,  0,  1},
+					      { 0,  1,  0,  0} }, board);
 		
+	}
+
+	@Test(expected = AssertionError.class)
+	public void test_WhenArraySizeIs3_ThenArrayIsNotSameAsExpected() {
+		int[][] board = nQueenProblem.nQueenSolution( 3);
+		assertArrayEquals(new int[][] {{0, 0, 0},
+						{0, 0, 0}, 
+						{0, 0, 0}}, board);
 	}
 
 	@Test
 	public void testSecond() {
-		assertEquals(true, nQueenProblem.nQueen(new int [][] {
-            { 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0 } }, 0, 8));
+		int[][] board = nQueenProblem.nQueenSolution( 8);
+		assertArrayEquals(new int [][] {
+				  { 1,  0,  0,  0,  0,  0,  0,  0},
+				  { 0,  0,  0,  0,  0,  0,  1,  0},
+				  { 0,  0,  0,  0,  1,  0,  0,  0},
+				  { 0,  0,  0,  0,  0,  0,  0,  1},
+				  { 0,  1,  0,  0,  0,  0,  0,  0},
+				  { 0,  0,  0,  1,  0,  0,  0,  0},
+				  { 0,  0,  0,  0,  0,  1,  0,  0},
+				  { 0,  0,  1,  0,  0,  0,  0,  0} }, board);
 		
 	}
 
