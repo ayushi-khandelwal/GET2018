255055ab30b30c675d58d31b37915f84a0f8b8af
Ayushi Khandelwal <41181775+meta-ayushi-khandelwal@users.noreply.github.com>
2018-08-01 01:44:37 -0700

    Update Point.java

--- PF/SCF-Assignment-9/graphics_library/Point.java
+++ PF/SCF-Assignment-9/graphics_library/Point.java
@@ -1,11 +1,37 @@
 package SCF_Assignment_9.graphics_library;
 
-public class Point {
-	public double x;
-	public double y;
+import graphics_library.Screen.ScreenException;
 
-	public Point(double x, double y) {
-		this.x = x;
-		this.y = y;
+/**
+ * This Circle class implements Shape interface
+ * 
+ * @author Ayushi Khandelwal
+ *
+ */
+public class Point {
+	private final double xPoint;
+	private final double yPoint;
+
+	public Point(double xPoint, double yPoint) throws ScreenException {
+		this.xPoint = xPoint;
+		this.yPoint = yPoint;
+		if (!checkLocation()) {
+			throw new ScreenException("Point out of Screen Size");
+		}
+	}
+
+	boolean checkLocation() {
+		if (xPoint >= 0 && xPoint <= 1920 && yPoint >= 0 && yPoint <= 1080) {
+			return true;
+		}
+		return false;
+	}
+
+	public double getxPoint() {
+		return xPoint;
+	}
+
+	public double getyPoint() {
+		return yPoint;
 	}
 }
