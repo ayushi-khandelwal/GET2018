53f63f1db2d2cee22489aac35e6db64154d3344a
Ayushi Khandelwal <41181775+meta-ayushi-khandelwal@users.noreply.github.com>
2018-08-01 01:42:57 -0700

    Update FactoryShape.java

--- PF/SCF-Assignment-9/graphics_library/FactoryShape.java
+++ PF/SCF-Assignment-9/graphics_library/FactoryShape.java
@@ -1,36 +1,52 @@
 package SCF_Assignment_9.graphics_library;
-
-import java.awt.Rectangle;
-import java.util.ArrayList;
-import java.util.List;
-
 import SCF_Assignment_9.graphics_library.Shapes.Circle;
 import SCF_Assignment_9.graphics_library.Shapes.Rectangle;
 import SCF_Assignment_9.graphics_library.Shapes.Shape;
 import SCF_Assignment_9.graphics_library.Shapes.Square;
 import SCF_Assignment_9.graphics_library.Shapes.Triangle;
+import graphics_library.Screen.ScreenException;
 
+import java.util.Date;
+import java.util.List;
+
+/**
+ * This Circle class implements Shape interface
+ * 
+ * @author Ayushi Khandelwal
+ *
+ */
 public class FactoryShape {
-	
-	public static Shape createShape(Shape.ShapeType shapeType, Point pointMin, Point pointMax, ArrayList<Double> list) {
-		
-		switch(shapeType) {
-			case Circle:
-				return new Circle(list.get(0), pointMax);
-
-			case Rectangle:
-				return new Rectangle(list.get(0), list.get(1), pointMin, pointMax);
-
-			case Square:
-				return new Square(list.get(0), pointMin, pointMax);
-
-			case Triangle:
-				Point point = new Point(0.0, 0.0);
-				return new Triangle(list.get(0), point, pointMin, pointMax);
-		default:
-			break;
+	static Shape createShape(ShapeType shapeType, Point point, List<Double> parameters) throws ScreenException {
+		if (shapeType == null || point == null || parameters == null) {
+			throw new NullPointerException("Arguments in addShape can't be Null");
+		}
+		switch (shapeType) {
+		case Circle:
+			if (parameters.size() != 1) {
+				throw new ScreenException("Circle Radius should be Given");
+			}
+			return new Circle(parameters.get(0), point, new Date());
 				
+		case Rectangle:
+			if (parameters.size() != 2) {
+				throw new ScreenException("Rectangle Width and Height should be Given");
+			}
+			return new Rectangle(parameters.get(0), parameters.get(1), point, new Date());
+				
+		case Square:
+			if (parameters.size() != 1) {
+				throw new ScreenException("Square Width should be Given");
+			}
+			return new Square(parameters.get(0), point, new Date());
+				
+		case Triangle:
+			if (parameters.size() != 3) {
+				throw new ScreenException("Triangle Base Height and one Side Should be Given should be Given");
+			}
+			return new Triangle(parameters.get(0), parameters.get(1), parameters.get(2), point, new Date());
+				
+		default:
+			return null;
 		}
 	}
-
 }
