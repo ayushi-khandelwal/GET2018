a9c44ce8f9c71567bdbe1379cfba9ee116a94f61
Ayushi Khandelwal <41181775+meta-ayushi-khandelwal@users.noreply.github.com>
2018-08-06 14:18:42 +0530

    Update PostfixEvaluatorJUnitTest.java

--- DS/ds_Assignment_2/PostfixEvaluator/PostfixEvaluatorJUnitTest.java
+++ DS/ds_Assignment_2/PostfixEvaluator/PostfixEvaluatorJUnitTest.java
@@ -1,7 +1,6 @@
-package ds_Assignment_2.PostfixEvaluator;
+package ds_Assignment_2.Q1_PostfixEvaluator;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class PostfixEvaluatorJUnitTest {
@@ -9,21 +8,32 @@ public class PostfixEvaluatorJUnitTest {
 	PostfixEvaluator postfixEvaluator = new PostfixEvaluator();
 	
 	@Test
-	public void test_WhenReturnValidOutput() {
+	public void test_WhenEvaluate_ReturnValidOutput() {
 		String actualExpression = "1 2 3 * + 4 -";
-		assertEquals(3, postfixEvaluator.evaluate(actualExpression));
+		assertEquals(3, Integer.parseInt(postfixEvaluator.evaluate(actualExpression)));
 	}
 
 	@Test
-	public void test_WhenExtraOperatorRemainsInStack_ReturnNegativeValue() {
+	public void test_WhenEvaluate_ReturnNegativeValue() {
+		String actualExpression = "2 3 1 * + 9 -";
+		assertEquals(-4, Integer.parseInt(postfixEvaluator.evaluate(actualExpression)));
+	}
+
+	@Test
+	public void test_WhenEvaluateOnPrferenceBasis_ReturnValidOutput() {
+		String actualExpression = "100 200 + 2 / 5 * 7 +";
+		assertEquals(757, Integer.parseInt(postfixEvaluator.evaluate(actualExpression)));
+	}
+    
+	@Test
+	public void test_WhenExtraOperatorRemainsInStack_ReturnNull() {
 		String actualExpression = "1 2 3 * + -";
-		assertEquals(-1, postfixEvaluator.evaluate(actualExpression));
+		assertEquals(null, postfixEvaluator.evaluate(actualExpression));
 	}
 
 	@Test
-	public void test_WhenExtraOperandRemainInStack_ReturnNegativeValue() {
+	public void test_WhenExtraOperandRemainInStack_ReturnNull() {
 		String actualExpression = "1 2 3 * + 4";
-		assertEquals(-1, postfixEvaluator.evaluate(actualExpression));
+		assertEquals(null, postfixEvaluator.evaluate(actualExpression));
 	}
-
 }
