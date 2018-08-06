61106398e96e51783e681ac37d7222d2d0d66a6f
Ayushi Khandelwal <41181775+meta-ayushi-khandelwal@users.noreply.github.com>
2018-08-06 14:16:56 +0530

    Update and rename QueueUsingArrayJUnitTest.java to CircularQueueJUnitTest.java

--- DS/ds_Assignment_2/CircularQueue/CircularQueueJUnitTest.java
+++ DS/ds_Assignment_2/CircularQueue/CircularQueueJUnitTest.java
@@ -1,24 +1,23 @@
-package ds_Assignment_2.CircularQueue;
+package ds_Assignment_2.Q3_CircularQueue;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
-public class QueueUsingArrayJUnitTest {
+public class CircularQueueUnitTest {
 
-	QueueUsingArray queue = new QueueUsingArray(3);
+	CircularQueue queue = new CircularQueue(3);
 
 	@Test
 	public void test_enqueue_WhenElementIsEnqueued_ReturnTrue() {
 		assertEquals(true, queue.enqueue(1));
 	}
-	
-	@Test
+
+	@Test	
 	public void test_enqueue_WhenQueueIsFull_ReturnFalse() {
-			assertEquals(true, queue.enqueue(1));
-			assertEquals(true, queue.enqueue(2));
-			assertEquals(true, queue.enqueue(3));
-			assertEquals(false, queue.enqueue(4));
+		assertEquals(true, queue.enqueue(1));
+		assertEquals(true, queue.enqueue(2));
+		assertEquals(true, queue.enqueue(3));
+		assertEquals(false, queue.enqueue(4));
 	}
 
 	@Test
@@ -28,17 +27,17 @@ public class QueueUsingArrayJUnitTest {
 		assertEquals(true, queue.enqueue(4));
 		assertEquals(2, queue.dequeue());
 	}
-	
+
 	@Test
 	public void test_dequeue_WhenqueueIsEmpty_ReturnFalse() {
 		assertEquals(-1, queue.dequeue());
 	}
-	
+
 	@Test
 	public void test_isEmpty_WhenqueueIsEmpty_ReturnTrue() {
 		assertEquals(true, queue.isEmpty());
 	}
-	
+
 	@Test
 	public void test_dequeue_WhenqueueIsNotEmpty_ReturnFalse() {
 		assertEquals(true, queue.enqueue(2));
@@ -46,22 +45,22 @@ public class QueueUsingArrayJUnitTest {
 		assertEquals(true, queue.enqueue(4));
 		assertEquals(false, queue.isEmpty());
 	}
-    @Test
-    public void test_peek_WhenQueueIsEmpty_ReturnNegativeValue() {
-        assertEquals(-1, queue.peek());
-    }
-    
-    @Test
-    public void test_peek_WhenQueueHasElements_ReturnPeekValue() {
-        assertEquals(true, queue.enqueue(3));
-        assertEquals(true, queue.enqueue(2));
-        assertEquals(3, queue.dequeue());
-        assertEquals(true, queue.enqueue(4));
-        assertEquals(2, queue.dequeue());
-        assertEquals(4, queue.dequeue());
-        assertEquals(-1, queue.peek());
-    }
-    
+	@Test
+	public void test_peek_WhenQueueIsEmpty_ReturnNegativeValue() {
+		assertEquals(-1, queue.peek());
+	}
+
+	@Test
+	public void test_peek_WhenQueueHasElements_ReturnPeekValue() {
+		assertEquals(true, queue.enqueue(3));
+		assertEquals(true, queue.enqueue(2));
+		assertEquals(3, queue.dequeue());
+		assertEquals(true, queue.enqueue(4));
+		assertEquals(2, queue.dequeue());
+		assertEquals(4, queue.dequeue());
+		assertEquals(-1, queue.peek());
+	}
+
 	@Test
 	public void test_dequeue_WhenqueueIsFull_ReturnTrue() {
 		assertEquals(true, queue.enqueue(2));
@@ -73,15 +72,15 @@ public class QueueUsingArrayJUnitTest {
 	public void test_dequeue_WhenqueueIsNotFull_ReturnFalse() {
 		assertEquals(false, queue.isFull());
 	}
-	
+
 	@Test
-    public void test_WhenOpeartionsAreRandom() {
-        assertEquals(true, queue.enqueue(3));
-        assertEquals(true, queue.enqueue(2));
-        assertEquals(true, queue.enqueue(5));
-        assertEquals(3, queue.dequeue());
-        assertEquals(2, queue.dequeue());
-        assertEquals(true, queue.enqueue(6));
-        assertEquals(true, queue.enqueue(7));
-    }
+	public void test_WhenOpeartionsAreRandom() {
+		assertEquals(true, queue.enqueue(3));
+		assertEquals(true, queue.enqueue(2));
+		assertEquals(true, queue.enqueue(5));
+		assertEquals(3, queue.dequeue());
+		assertEquals(2, queue.dequeue());
+		assertEquals(true, queue.enqueue(6));
+		assertEquals(true, queue.enqueue(7));
+	}
 }
