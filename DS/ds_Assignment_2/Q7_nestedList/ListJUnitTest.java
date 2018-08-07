9bc33c2628881a8ef78739b7c9bf8f0c03dd249c
Ayushi Khandelwal <41181775+meta-ayushi-khandelwal@users.noreply.github.com>
2018-08-07 14:39:04 +0530

    Add files via upload

--- DS/ds_Assignment_2/Q7_nestedList/ListJUnitTest.java
+++ DS/ds_Assignment_2/Q7_nestedList/ListJUnitTest.java
@@ -28,49 +28,43 @@ ListOLists listOLists = new ListOLists();
         
         listOLists.addCustomList(innerList11, innerList12, innerList21, innerList22, innerList31, innerList32);
     }
-    
     @Test(expected = AssertionError.class)
-    public void test_LargestValue_SingleH_ReturnInvalidOutput() { 
+    public void givenNestedList_whenElementToBeFoundAt_H_thenAssertionErrorExpectedAsListOccursAtHead() {
         assertEquals(null, listOLists.getValue("H"));
     }
 
     @Test(expected = AssertionError.class)
-    public void test_LargestValue_SingleT_ReturnInvalidOutput() { 
+    public void givenNestedList_whenElementToBeFoundAt_T_thenAssertionErrorExpectedAsListOccursAtTail() {
         assertEquals(null, listOLists.getValue("T"));
     }
 
     @Test(expected = AssertionError.class)
-    public void test_LargestValue_EndsWithT_ReturnInvalidOutput() { 
+    public void  givenNestedList_whenElementToBeFoundAt_HHT_with_T_AtTheEnd_thenAssertionErrorExpected() {
         assertEquals(null, listOLists.getValue("HHT"));
     }
 
     @Test
-    public void test_LargestValue__ReturnValidOutput() { 
+    public void  givenNestedList_whenElementToBeFoundAt_HHH_thenReturnsValidOutput() {
         assertEquals(1, Integer.parseInt(listOLists.getValue("HHH")));
     }
 
     @Test
-    public void test_LargestValue_HHTH_ReturnValidOutput() { 
+    public void givenNestedList_whenElementToBeFoundAt_HHTH_thenReturnsValidOutput() {
         assertEquals(2, Integer.parseInt(listOLists.getValue("HHTH")));
     }
 
     @Test
-    public void test_LargestValue_HHTTH_ReturnValidOutput() { 
+    public void givenNestedList_whenElementToBeFoundAt_HHTTH_thenReturnsValidOutput() {
         assertEquals(3, Integer.parseInt(listOLists.getValue("HHTTH")));
     }
 
     @Test
-    public void test_LargestValue_HHTTTH_ReturnValidOutput() { 
+    public void givenNestedList_whenElementToBeFoundAt_HHTTTH_thenReturnsValidOutput() {
         assertEquals(4, Integer.parseInt(listOLists.getValue("HHTTTH")));
     }
 
     @Test
-    public void test_LargestValue_HHTH_ReturnInvalidOutput() { 
+    public void givenNestedList_whenElementToBeFoundAt_HTHH_thenReturnsValidOutput() {
         assertEquals(5, Integer.parseInt(listOLists.getValue("HTHH")));
-    }
-
-    @Test(expected = AssertionError.class)
-    public void test_LargestValue_IfInputIsAccessingArrayList_ReturnInvalidOutput() { 
-        assertEquals(null, listOLists.getValue("HTH"));
     }
 }
