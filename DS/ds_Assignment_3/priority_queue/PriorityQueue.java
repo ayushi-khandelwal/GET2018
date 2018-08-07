ab76f424c37c1ffd270436cd13f1f2be4343fc27
Ayushi Khandelwal <41181775+meta-ayushi-khandelwal@users.noreply.github.com>
2018-08-08 01:17:18 +0530

    Add files via upload

--- DS/ds_Assignment_3/priority_queue/PriorityQueue.java
+++ DS/ds_Assignment_3/priority_queue/PriorityQueue.java
@@ -5,63 +5,89 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Objects;
 
+/**
+ * @author Ayushi Khandelwal
+ *
+ */
 public class PriorityQueue implements Queue {
-
+    
+    //HashMap is used to store item and its priority
     private HashMap< String, Integer> map;
     
+    //default constructor to initialize HashMap
     public PriorityQueue() {
         map = new HashMap<>();
     }
     
+    //inserts item and its priority in HashMap
     @Override
     public boolean enqueue(String item, int priority) {
-        if(priority < 0){
-            return false;
+        //if priority is negative then throws AssertionError
+        if(priority < 0 || item == ""){
+            throw new AssertionError();
         }
         
         map.put(item, priority);
         return true;
     }
-
+    
+    //delete item of highest priority from HashMap
+    @SuppressWarnings("rawtypes")
     @Override
-    public int getHighestPriority() {
-        int largestPriority = -1;
-        
-        for (Object obj: map.entrySet() ) {
-            Map.Entry entry = (Map.Entry) obj;
-            if(largestPriority < (int) entry.getValue()) {
-                largestPriority = (int) entry.getValue();
-            }
-        }
-        return largestPriority;
-    }
-
-    @Override
-    public String getHighestPriorityItem() {
+    public String dequeueHighestPriorityItem() {
         int largestPriority = getHighestPriority();
         
-        for (Entry entry : map.entrySet()) { 
-            if (Objects.equals(largestPriority, entry.getValue())) { 
-                return (String) entry.getKey(); 
-            } 
-        } return null; 
-    }
-            
-    @Override
-    public String dequeueHighestPriority() {
-        int largestPriority = getHighestPriority();
         for (Object obj: map.entrySet() ) {
             Map.Entry entry = (Map.Entry) obj;
             if(largestPriority == (int) entry.getValue()) {
                 map.remove(entry.getKey());            
-                System.out.println(entry.getKey() + " " + entry.getValue());
                 return (String) entry.getKey();
             }
         }
         return null;
     }
     
-    @SuppressWarnings("unchecked")
+    //gives highest priority among priorities of all items present in HashMap
+    @SuppressWarnings("rawtypes")
+    @Override
+    public int getHighestPriority() {
+        if(!isEmpty()) {
+            int largestPriority = -1;
+            
+            //retrieve HashMap to get highest priority
+            for (Object obj: map.entrySet() ) {
+                Map.Entry entry = (Map.Entry) obj;
+                if(largestPriority < (int) entry.getValue()) {
+                    largestPriority = (int) entry.getValue();
+                }
+            }
+            return largestPriority;
+        }
+        throw new AssertionError();
+    }
+
+    //gives item having highest priority among all the items present in HashMap
+    @SuppressWarnings("rawtypes")
+    @Override
+    public String getHighestPriorityItem() {
+        int largestPriority = getHighestPriority();
+        
+        //retrieves key from value
+        for (Entry entry : map.entrySet()) { 
+            if (Objects.equals(largestPriority, entry.getValue())) { 
+                return (String) entry.getKey(); 
+            } 
+        } return null; 
+    }
+    
+    @Override
+    public boolean isEmpty() {
+        if(map.isEmpty())
+            return true;
+        return false;
+    }
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
     public String getItem() {
         int largestPriority = getHighestPriority();
         
--- DS/ds_Assignment_3/priority_queue/Queue.java
+++ DS/ds_Assignment_3/priority_queue/Queue.java
@@ -1,8 +1,9 @@
 package ds_Assignment_3.priority_queue;
 
 public interface Queue {
+    public boolean isEmpty();
     public boolean enqueue(String item, int priority);
+    public String dequeueHighestPriorityItem();
     public int getHighestPriority();
-    public String dequeueHighestPriority();
     public String getHighestPriorityItem();
 }
