68a57f05762749314fbbd1128a3464e767042010
Ayushi Khandelwal <41181775+meta-ayushi-khandelwal@users.noreply.github.com>
2018-08-21 14:10:54 +0530

    Add files via upload

--- DBMS/DBMS_Session_4/DBMS_Session_4-Assignment_1.sql
+++ DBMS/DBMS_Session_4/DBMS_Session_4-Assignment_1.sql
@@ -2,39 +2,44 @@ USE StoreFront;
 
 /* 1 Create a function to calculate number of orders in a month. 
 Month and year will be input parameter to function. */
+DROP FUNCTION IF EXISTS getNumberOfOrdersInMonth;
+
 DELIMITER $$
 
 CREATE FUNCTION getNumberOfOrdersInMonth (Order_Month INT, Order_Year INT)
-RETURNS INT DETERMINISTIC
+    RETURNS INT DETERMINISTIC
 
-BEGIN
-    DECLARE OrderCount INT;
-    SET     OrderCount =   (SELECT	count(*)
-                            FROM 	Orders
-                            WHERE 	MONTH(Orders.Order_Date) = Order_Month AND
-									YEAR(Orders.Order_Date) = Order_Year);
-    RETURN (OrderCount);
-END $$
+    BEGIN
+        DECLARE OrderCount INT;
+        SET     OrderCount =   (SELECT	COUNT(*)
+                                FROM 	    Orders
+                                WHERE 	MONTH(Orders.Order_Date) = Order_Month AND
+                                        YEAR(Orders.Order_Date) = Order_Year);
+        RETURN (OrderCount);
+    END $$
+DELIMITER ;
 
 SELECT getNumberOfOrdersInMonth(8, 2018) AS total_order;
 
-#2 Create a function to return month in a year having maximum orders. Year will be input parameter.
-DROP FUNCTION if exists getMonthOfMaximumOrders;
+/* 2 Create a function to return month in a year having maximum orders. 
+Year will be input parameter. */
+DROP FUNCTION IF EXISTS StoreFront.getMonthOfMaximumOrders;
+
 DELIMITER $$
 
-CREATE FUNCTION getMonthOfMaximumOrders (Order_Year VARCHAR(6555))
-RETURNS INT DETERMINISTIC
+CREATE FUNCTION getMonthOfMaximumOrders (Order_Year INT)
+    RETURNS INT DETERMINISTIC
 
-BEGIN
-	DECLARE Max_Order_Month INT;
-    SET 	Max_Order_Month = 	(SELECT		MONTH(Order_Date)
-								 FROM		Orders
+    BEGIN
+        DECLARE Max_Order_Month INT;
+        SET 	Max_Order_Month = (SELECT   MONTH(Order_Date)
+                                 FROM     Orders
                                  WHERE		YEAR(Orders.Order_Date) = Order_Year
-                                 ORDER BY	MONTH(Order_Date) DESC
+                                 ORDER BY	getNumberOfOrdersInMonth(MONTH(Order_Date),YEAR(Orders.Order_Date)) DESC
                                  LIMIT		1);
-                                 
-	RETURN	Max_Order_Month;
-END $$
-	
+                                     
+        RETURN	(Max_Order_Month);
+    END $$
+DELIMITER ;
 
-SELECT getMonthOfMaximumOrders(2018) AS Maximum_Order_Month;
+SELECT GETMONTHOFMAXIMUMORDERS(2018) AS maximum_Order_Month;
