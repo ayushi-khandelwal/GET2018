2c66d2abed1dc22dc923ed40c3bc6b04fd64cc1f
Ayushi Khandelwal <41181775+meta-ayushi-khandelwal@users.noreply.github.com>
2018-10-11 01:23:33 +0530

    Update repeatify.js

--- UI/UI_Session_8/Question-5_String/repeatify.js
+++ UI/UI_Session_8/Question-5_String/repeatify.js
@@ -1,8 +1,8 @@
 "use strict";
 
-String.prototype.repeatifyString = function(countForRepeatation = 0) {
+String.prototype.repeatifyString = function(countForRepeatation) {
     let returnString = "";
-    for(let i=0;i<countForRepeatation;i++) {
+    for(let i=0; i<countForRepeatation; i++) {
         returnString += this;
     }
     return returnString;
@@ -10,9 +10,9 @@ String.prototype.repeatifyString = function(countForRepeatation = 0) {
 
 function takeString() {
     let string = prompt("Enter the String:");
-    if(string !=null && string != "") {
+    if(string != null && string != "") {
         let countForRepeatation = prompt("Enter the repeatation count:");
-        if(countForRepeatation !=null && countForRepeatation != "") {
+        if(countForRepeatation != null && countForRepeatation != "") {
             alert(string.repeatifyString(countForRepeatation));
         } else {
             alert("You have cancelled the operation !");
